5dc468930145a3b3f51bb95d896b7a6731ea5ae2
Jonathan Stacks <jonstacks13@gmail.com>
2016-09-16 10:31:56 -0500

    Storage Manager Size, Count, and String functions

--- awsutils/storage_stat_manager.go
+++ awsutils/storage_stat_manager.go
@@ -1,6 +1,11 @@
 package awsutils
 
-import "github.com/aws/aws-sdk-go/service/s3"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/service/s3"
+)
 
 // Storage Classes
 const (
@@ -20,13 +25,17 @@ var storageClasses = []string{
 // StorageStatsManager is used to manage the stats for different AWS Storage
 // Classes in an easy to use interface.
 type StorageStatsManager struct {
-	Stats map[string]*StorageStat
+	Stats   map[string]*StorageStat
+	classes *[]string // This is just here so we can keep track of the ordering
 }
 
 // NewStorageStatsManager returns a new & initialized StorageStatsManger
 func NewStorageStatsManager() *StorageStatsManager {
-	sm := &StorageStatsManager{Stats: make(map[string]*StorageStat)}
-	for _, s := range storageClasses {
+	sm := &StorageStatsManager{
+		Stats:   make(map[string]*StorageStat),
+		classes: &storageClasses,
+	}
+	for _, s := range *sm.classes {
 		sm.Stats[s] = NewStorageStat()
 	}
 	return sm
@@ -35,4 +44,32 @@ func NewStorageStatsManager() *StorageStatsManager {
 // AddObject adds an object to the storage stat it belongs to
 func (sm *StorageStatsManager) AddObject(obj *s3.Object) {
 	sm.Stats[*obj.StorageClass].AddObject(*obj.Size)
+}
+
+// Size returns the total size across all storage stats
+func (sm *StorageStatsManager) Size() int64 {
+	size := int64(0)
+	for _, v := range sm.Stats {
+		size += v.Size
+	}
+	return size
+}
+
+// Count returns the total count across all storage stats
+func (sm *StorageStatsManager) Count() int64 {
+	count := int64(0)
+	for _, v := range sm.Stats {
+		count += v.Count
+	}
+	return count
+}
+
+func (sm *StorageStatsManager) String() string {
+	stats := make([]string, 0, len(sm.Stats))
+
+	for _, k := range *sm.classes {
+		stats = append(stats, fmt.Sprintf("%s%s", k, sm.Stats[k]))
+	}
+	x := strings.Join(stats, ", ")
+	return fmt.Sprintf("[%s]", x)
 }
--- awsutils/storage_stats_manager_test.go
+++ awsutils/storage_stats_manager_test.go
@@ -1,6 +1,7 @@
 package awsutils
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,15 +10,15 @@ import (
 func TestStorageStatsManager(t *testing.T) {
 	sm := NewStorageStatsManager()
 
-  // All Storage Stats should be initialized
-  for _, s := range storageClasses {
-    assert.NotNil(t, sm.Stats[s])
-  }
+	// All Storage Stats should be initialized
+	for _, s := range storageClasses {
+		assert.NotNil(t, sm.Stats[s])
+	}
 }
 
-// func ExampleStorageStatsManager() {
-// 	sm := NewStorageStatsManager()
-//
-// 	// Output:
-// 	// (count: 3, size: 6046)
-// }
+func ExampleStorageStatsManager() {
+	sm := NewStorageStatsManager()
+	fmt.Println(sm)
+	// Output:
+	// [STANDARD(count: 0, size: 0), STANDARD_IA(count: 0, size: 0), REDUCED_REDUNDANCY(count: 0, size: 0), GLACIER(count: 0, size: 0)]
+}
